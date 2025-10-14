86edae72f9b5f8c514e91adaaaab91ff92b50105
Gavan <gavanbess@gmail.com>
2025-10-13 22:35:45 -0700

    made it better

--- lib/NetworkTables4/NT4.gd
+++ lib/NetworkTables4/NT4.gd
@@ -76,14 +76,11 @@ class NT4_Client:
 	func update():
 		if ws != null:
 			ws.poll()
-			if ws.get_available_packet_count() > 0:
-				var packet = ws.get_packet()
-				if ws.was_string_packet():
-					print(packet.get_string_from_ascii())
+			for i in range(ws.get_available_packet_count()):
+				ws_on_recieve_packet(ws.get_packet(), ws.was_string_packet())
 			if (serverConnectionRequested and !serverConnected):
 				if ws.get_ready_state() == WebSocketPeer.STATE_OPEN:
-					serverConnected = true
-					serverConnectionRequested = false
+					ws_on_server_connect()
 
 	func subscribe(topicPatterns: Array[String], prefixMode: bool, sendAll: bool = false, periodic: float = 0.1) -> int:
 		var newSub = NT4_Subscription.new()
@@ -95,10 +92,33 @@ class NT4_Client:
 
 		subscriptions.set(newSub.uid, newSub)
 		if (serverConnected):
-			var payload = JSON.stringify([{"method": "subscribe", "params": newSub.toSubscribeObj()}])
-			ws.send_text(payload)
+			ws_send_json("subscribe", newSub.toSubscribeObj())
 
 		return newSub.uid
-
+		
+	func ws_send_json(method: String, params: Variant):
+		if (ws and serverConnected):
+			ws.send_text(JSON.stringify([{
+				"method": method,
+				"params": params
+			}]))
+	
+	func ws_on_server_connect():
+		serverConnected = true
+		serverConnectionRequested = false
+		
+		
+	
+	func ws_on_recieve_packet(packet: PackedByteArray, was_string: bool):
+		if was_string:
+			var data = JSON.parse_string(packet.get_string_from_ascii())
+			if !data:
+				push_warning("NT4: ignoring packet because json did not parse")
+			else:
+				print(data)
+		else:
+			# this means its msgpack (annoying but cool)
+			pass
+		
 	func getNewUID():
 		return rng.randi_range(1, 999999999)
