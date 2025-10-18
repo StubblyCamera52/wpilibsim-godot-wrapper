99c917e14944bccdb73eb3cc92a0d31e77e88eab
Gavan <gavanbess@gmail.com>
2025-10-18 10:42:35 -0700

    started adding support for the AdvantageScope model format
    doing this because my glb files are too big to upload to github

--- main.gd
+++ main.gd
@@ -4,9 +4,15 @@ var socket = WebSocketPeer.new()
 
 var initializedsocket = false
 
-var nt_client: NT4.NT4_Client
+var nt_client: NT4.NT4_Client = null
 
-@onready var botComponents = []
+@onready var bot: Node3D = $bot
+
+var bot_model: Array[Node3D] = []
+var bot_model_zeroed_positions: Array = []
+
+var gltf_document_load = GLTFDocument.new()
+var gltf_state_load = GLTFState.new()
 
 func on_topic_announced(topic: NT4.NT4_Topic):
 	pass
@@ -21,13 +27,31 @@ func on_new_topic_data(topic: NT4.NT4_Topic, timestamp_us: int, value: Variant):
 		on_robot_move(pose)
 
 func _ready():
-	nt_client = NT4.NT4_Client.new("godot-sim", "ws://localhost:5810/nt/godotsim")
-	nt_client.on_topic_announce = on_topic_announced
-	nt_client.on_new_topic_data = on_new_topic_data
-	nt_client.connect_ws()
-	while !nt_client.serverConnected:
-		await get_tree().process_frame
-	nt_client.subscribe(["/AdvantageKit/RealOutputs/FieldSimulation/RobotPose"], false, false, 0.05)
+	var bot_config = JSON.parse_string(FileAccess.open("/Users/gavanbess/3681-sim/models/Robot_2025/config.json", FileAccess.READ).get_as_text())
+	if bot_config:
+		var error = gltf_document_load.append_from_file("/Users/gavanbess/3681-sim/models/Robot_2025/model.glb", gltf_state_load)
+		if error == OK:
+			bot_model.append(gltf_document_load.generate_scene(gltf_state_load))
+			bot_model[0].position = Vector3(bot_config.position[0], bot_config.position[1], bot_config.position[2])
+			for i in range(bot_config.rotations.size()):
+				match bot_config.rotations[i].axis:
+					"x":
+						pass
+					"y":
+						pass
+					"z":
+						pass
+			bot.add_child(bot_model[0])
+		else:
+			push_error("Couldn't load glTF scene (error code: %s)." % error_string(error))
+	
+	#nt_client = NT4.NT4_Client.new("godot-sim", "ws://localhost:5810/nt/godotsim")
+	#nt_client.on_topic_announce = on_topic_announced
+	#nt_client.on_new_topic_data = on_new_topic_data
+	#nt_client.connect_ws()
+	#while !nt_client.serverConnected:
+		#await get_tree().process_frame
+	#nt_client.subscribe(["/AdvantageKit/RealOutputs/FieldSimulation/RobotPose"], false, false, 0.05)
 	#nt_client.subscribe(["/AdvantageKit/RealOutputs/AScope/componentPoses"], false, false, 0.05)
 	
 
